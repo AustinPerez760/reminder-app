5262ceb2eb5c41fcc781b4fbdc054c422266cfa1
Austin Perez <austin.perez760@gmail.com>
2025-02-07 17:15:49 -0600

    styled text box and button

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,9 +1,17 @@
 export default function AddTodoForm() {
 	return (
 		<form>
-			<h2 className='font-medium text-red-700'>Add task</h2>
-			<input type='text' className='h-[45px] border border-black/12%' />
-			<button type='submit'> Add to list </button>
+			<h2 className='font-medium text-sky-400'>Add task</h2>
+			<input
+				type='text'
+				className='h-[45px] border border-sky-400 rounded-[5px] my-[9px] text-[14px] block w-full px-[14px]'
+			/>
+			<button
+				type='submit'
+				className='h-[45px] bg-amber-100 w-full text-sky-400 rounded-[8px] cursor-pointer hover:bg-amber-200'>
+				{' '}
+				Add to list{' '}
+			</button>
 		</form>
 	);
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import AddTodoForm from './AddTodoForm';
 
 export default function Sidebar() {
 	return (
-		<section className='col-[2/3] row-[2/3] bg-amber-900 border-l border-sky-400/[0.8] '>
+		<section className='col-[2/3] row-[2/3] bg-white border-l border-sky-400/[0.8] px-[25px] py-[18px] pb-[28px] '>
 			<AddTodoForm />
 		</section>
 	);
